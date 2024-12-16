8d1303be23c2d2b0b0fe43957200c57fd307f4d7
Md Abu Nasim <68380516+abu-nasim-lomani@users.noreply.github.com>
2024-12-16 11:18:43 +0600

    Delete BookNest - FC directory

--- BookNest - FC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookNest.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
